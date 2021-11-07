d22d94984c985ac34614013e670720056b18e949
Ahusik <93037845+Ahusik@users.noreply.github.com>
2021-11-07 15:01:54 +0100

    Create wyklad 2 zad 4.cpp

---
+++ wyklad 2 zad 4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+int liczba=10;
+
+cout<<"liczba :"<<liczba<<endl;
+
+cout<<"Podwojona"<<(liczba*2)<<endl;
+
+cout<<"Do Kwadratu"<<(liczba*liczba)<<endl;
+
+    return 0;
+}
