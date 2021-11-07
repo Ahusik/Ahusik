8d9762410a5ceab1171b93bb0fc07576b35b5091
Ahusik <93037845+Ahusik@users.noreply.github.com>
2021-11-07 14:34:49 +0100

    Add files via upload

---
+++ wykład 1 zad 1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+
+using namespace std;
+
+int cale;
+
+cout<<"Przelicznik cale na centymetry"<<endl;
+
+cout << "Podaj ilosc cali : ";
+
+cin >> cale;
+
+cout<<"Jest to : "<<(cale*2.54)<<"cm"<<endl;
+    return 0;
+}
