253b44a3820e0b4bafa94a3467e5e636e1c13675
Ahusik <93037845+Ahusik@users.noreply.github.com>
2021-11-07 14:46:25 +0100

    Create wyklad 2 zad 3.cpp

---
+++ wyklad 2 zad 3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int lata;
+
+cout<<"Program liczacy wiek z lat na wiek na dni"<<endl;
+
+cout<<"Ile masz Lat? : "<<endl;
+
+cin>>lata;
+
+cout<<"To "<<(lata*365)<<"Dni"<<endl;
+    return 0;
+}
