68d1470f21adf2ce3ba3c025cca1631f30cc1841
Ahusik <93037845+Ahusik@users.noreply.github.com>
2021-11-27 10:10:10 +0100

    Update while suma od 1 do 100.cpp

--- while suma od 1 do 100.cpp
+++ while suma od 1 do 100.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
 
+using namespace std;
+int main()
+{
+    int i=1, suma=0;
+    while(i<=100)
+    {
+        suma+=i;
+        i++;
+    }
+    cout << "Suma od 1 do 100 " <<" = " << suma << endl;
+}
