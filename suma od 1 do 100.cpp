2c0cffc88d1f3777151f1bfef0c522a1acf3a923
Ahusik <93037845+Ahusik@users.noreply.github.com>
2021-11-27 10:01:26 +0100

    Create suma od 1 do 100.cpp

---
+++ suma od 1 do 100.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int num=100, suma=0;
+
+
+    for (int i=1; i<=num; i++)
+    {
+
+        suma += i;
+    }
+    cout << "Suma od 1 do 100 " <<" = " << suma << endl;
+}
