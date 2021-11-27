ef4f21629bfa6d1591a42a6f7b9ff3ca616a2814
Ahusik <93037845+Ahusik@users.noreply.github.com>
2021-11-27 10:09:43 +0100

    Update for suma od 1 do 100.cpp

--- for suma od 1 do 100.cpp
+++ for suma od 1 do 100.cpp
@@ -4,11 +4,8 @@ using namespace std;
 int main()
 {
     int num=100, suma=0;
-
-
     for (int i=1; i<=num; i++)
     {
-
         suma += i;
     }
     cout << "Suma od 1 do 100 " <<" = " << suma << endl;
